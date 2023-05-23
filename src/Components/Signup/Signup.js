338fd5e5a33dbec36a80651ad149dafa64f8e455
Aswathi P J <aswathipjsreenilayam@gmail.com>
2023-05-23 21:01:35 +0530

    add signup authentication

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
+import { FirebaseContext } from '../../store/FirebaseContext';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
 
 export default function Signup() {
+const [username,setUsername]=useState('')
+const [email,setEmail]=useState('')
+const [phone,setPhone]=useState('')
+const [password,setPassword]=useState('')
+
+const {firebase}=useContext(FirebaseContext)
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  
+  const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    
+    userCredential.user.updateProfile({displayName:username})
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+  });
+
+  
+
+
+}
+
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -24,6 +57,8 @@ export default function Signup() {
           <input
             className="input"
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -34,6 +69,8 @@ export default function Signup() {
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -44,6 +81,8 @@ export default function Signup() {
           <input
             className="input"
             type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import {initializeApp} from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9Yt9naZ7C0UVDsbLPbZ2_7bBCXdu0zCE",
+    authDomain: "fir-bf8c5.firebaseapp.com",
+    projectId: "fir-bf8c5",
+    storageBucket: "fir-bf8c5.appspot.com",
+    messagingSenderId: "651399870794",
+    appId: "1:651399870794:web:eb62cb2814f3ff85f7a9b3",
+    measurementId: "G-XW8J1YNRKV"
+  };
+
+const FirebaseApp= initializeApp(firebaseConfig)
+const firebase = getFirestore(FirebaseApp) 
+
+export default firebase
+
