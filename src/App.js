adf43d2bd50ea36141a9f64a11e3d055dd33cf67
Aswathi P J <aswathipjsreenilayam@gmail.com>
2023-05-23 16:04:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+import Signup from './Pages/Signup';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+        <Route element={<Home/>} path="/">
+        
+        </Route>
+        <Route element={<Signup/>} path="/signup">
+        
+        </Route>
+
+
+        </Routes>
+        
+      
+      </Router>
+      
+      
+    </div>
+  );
+}
+
+export default App;
